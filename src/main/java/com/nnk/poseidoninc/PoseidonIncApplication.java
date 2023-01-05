0f3c692e8b614b3ea6b9ae6c20319aee76a2fd73
bertrand <bertrandn5@gmail.com>
2023-01-05 11:17:54 +0100

    Start Projet

---
+++ src/main/java/com/nnk/poseidoninc/PoseidonIncApplication.java
@@ -0,0 +1,13 @@
+package com.nnk.poseidoninc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonIncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoseidonIncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nnk/poseidoninc/PoseidonIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnk.poseidoninc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonIncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
