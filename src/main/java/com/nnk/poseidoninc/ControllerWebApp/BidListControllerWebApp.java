a007cca7dc3b4f0bf6c5314ac4c55917fb633be7
bertrand <bertrandn5@gmail.com>
2023-01-16 16:32:54 +0100

    add CurvePointControllerWebApp
    continue html

--- src/main/java/com/nnk/poseidoninc/ControllerWebApp/BidListControllerWebApp.java
+++ src/main/java/com/nnk/poseidoninc/ControllerWebApp/BidListControllerWebApp.java
@@ -2,12 +2,15 @@ package com.nnk.poseidoninc.ControllerWebApp;
 
 import com.nnk.poseidoninc.Model.Dto.BidListDto;
 import com.nnk.poseidoninc.Service.Implementation.BidListServiceImpl;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 
@@ -32,9 +35,47 @@ public class BidListControllerWebApp {
         return "bidList/list";
     }
 
+
+    @GetMapping(value = "/BidList/add")
+    public String addBidListPage(BidListDto bidListDto) {
+
+        return "bidList/add";
+    }
+
+    @PostMapping(value = "/BidList/add")
+    public String addBidList(@Validated BidListDto bidListDto) {
+        bidListService.create(bidListDto);
+
+        return "redirect:/BidList";
+    }
+
+
+    @GetMapping(value = "/BidList/update/{id}")
+    public String updatePage(
+            @PathVariable(value = "id") int bidListId,
+            Model model
+    ) {
+
+        BidListDto bidListDto = bidListService.findById(bidListId);
+
+        model.addAttribute("bidListDto", bidListDto);
+
+        return "bidList/update";
+    }
+
+    @PostMapping(value = "/BidList/update/{id}")
+    public String update(
+            @PathVariable(value = "id") int bidListId,
+            BidListDto bidListDto
+    ) {
+
+        bidListService.update(bidListDto, bidListId);
+        return "redirect:/BidList";
+    }
+
     @GetMapping(value = "/BidList/delete/{id}")
     public String delete(
-           @PathVariable(value = "id") int bidListId
+            @PathVariable(value = "id") int bidListId
     ) {
 
         bidListService.delete(bidListId);
---
+++ src/main/java/com/nnk/poseidoninc/ControllerWebApp/CurvePointControllerWebApp.java
@@ -0,0 +1,81 @@
+package com.nnk.poseidoninc.ControllerWebApp;
+
+import com.nnk.poseidoninc.Model.Dto.CurvePointDto;
+import com.nnk.poseidoninc.Service.Implementation.CurvePointServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class CurvePointControllerWebApp {
+
+    private CurvePointServiceImpl curvePointService;
+
+    public CurvePointControllerWebApp(CurvePointServiceImpl curvePointService) {
+        this.curvePointService = curvePointService;
+    }
+
+    @GetMapping(value = "/CurvePoint")
+    public String home(Model model) {
+        List<CurvePointDto> curvePointDtoList = curvePointService.findAll();
+
+        model.addAttribute("curvePointDtoList", curvePointDtoList);
+        model.addAttribute("user", "USER TESTA");
+
+        return "curvePoint/list";
+    }
+
+    @GetMapping("/CurvePoint/add")
+    public String addCurvePointPage(CurvePointDto curvePointDto) {
+
+        return "curvePoint/add";
+
+    }
+
+    @PostMapping("/CurvePoint/add")
+    public String addCurvePoint(@Validated CurvePointDto curvePointDto) {
+
+        curvePointService.create(curvePointDto);
+
+        return "redirect:/CurvePoint";
+    }
+
+    @GetMapping(value = "/CurvePoint/update/{id}")
+    public String updatePage(
+            @PathVariable(value = "id") int curePointId,
+            Model model) {
+        CurvePointDto curvePointDto = curvePointService.findById(curePointId);
+
+        model.addAttribute("curvePointDto", curvePointDto);
+
+        return "curvePoint/update";
+    }
+
+    @PostMapping(value = "/CurvePoint/update/{id}")
+    public  String update (
+            @PathVariable(value = "id") int curePointId,
+            @Validated CurvePointDto curvePointDto
+    ){
+
+        curvePointService.update(curvePointDto, curePointId);
+        return "redirect:/CurvePoint";
+
+    }
+
+
+    @GetMapping(value = "/CurvePoint/delete/{id}")
+    public String delete(
+            @PathVariable(value = "id") int curvePointId
+    ) {
+
+        curvePointService.delete(curvePointId);
+
+        return "redirect:/CurvePoint";
+    }
+}
