961ce56cde98310a4fdca9dcf1786bad5c1ed2bb
bertrand <bertrandn5@gmail.com>
2023-01-25 15:11:44 +0100

    add RsaKeyGenerator.java

---
+++ src/main/java/com/nnk/poseidoninc/RsaKeyGenerator.java
@@ -0,0 +1,32 @@
+package com.nnk.poseidoninc;
+
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class RsaKeyGenerator {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        byte[] pub = keyPair.getPublic().getEncoded();
+        byte[] pri = keyPair.getPrivate().getEncoded();
+
+        PemWriter pemWriter = new PemWriter(new OutputStreamWriter(new FileOutputStream("src/main/resources/certs/pub.pem")));
+        PemObject pemObject = new PemObject("PUBLIC KEY", pub);
+        pemWriter.writeObject(pemObject);
+        pemWriter.close();
+
+        PemWriter pemWriter2 = new PemWriter(new OutputStreamWriter(new FileOutputStream("src/main/resources/certs/pri.pem")));
+        PemObject pemObject2 = new PemObject("PRIVATE KEY", pri);
+        pemWriter2.writeObject(pemObject2);
+        pemWriter2.close();
+    }
+}
